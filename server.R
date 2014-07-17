5afa0712a44cec8b4a9d78d9a82b875f823fd91b
Ali Arsalan <scac_1041@hotmail.com>
2014-07-17 20:52:59 +0300

    encoding changed for UTF-8

--- server.R
+++ server.R
@@ -135,12 +135,13 @@ initialCorpus<- reactive({
 					userUpload<- reactive({ input$myPath })
 					filePath<- userUpload()$datapath
 					myData<- unlist(lapply(filePath,function(x)scan(file=x,what="character",sep="\n")))
+					Encoding(myData)<- "UTF-8"
 					myCorpus<- Corpus(VectorSource(myData))
 					return(myCorpus)
 					}
 				else{
 					myFile<- getURL(url=paste0(ghubURL,input$sampleCorpus,".txt"),ssl.verifypeer=FALSE)
-					txtFile<- scan(textConnection(myFile),sep="\n",what="character")
+					txtFile<- scan(textConnection(myFile, encoding="UTF-8"),sep="\n",what="character",fileEncoding="UTF-8",encoding="UTF-8")
 					myCorpus<- Corpus(VectorSource(txtFile))
 					return(myCorpus)
 					} 
